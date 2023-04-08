a9c3c54f9b059d79b131e4477c29d73fd5a1696f
noyon-podder <noyon.vai369@gmail.com>
2023-04-08 13:41:09 +0600

    features section add this commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Hero from '../component/Hero'
 import Work from '../component/Work'
+import Features from '../component/Features'
 
 const Home = () => {
   return (
     <div>
         <Hero/>
         <Work/>
+        <Features/>
     </div>
   )
 }
